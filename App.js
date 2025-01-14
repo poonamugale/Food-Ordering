727651d34bc93ce13c39f3e8af8071ad5214e8a7
Poonam <poonamugale1997@gmail.com>
2025-01-14 12:14:48 +0530

    Added some changes

--- App.js
+++ App.js
@@ -43,6 +43,7 @@ const resList = [
     type: "restaurant",
     data: {
       type: "F",
+      id: "1",
       name: "KFC",
       src: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2024/12/9/4c1906b4-f727-434b-8496-a2e669269dde_243625.JPG",
 
@@ -56,6 +57,7 @@ const resList = [
     type: "restaurant",
     data: {
       type: "F",
+      id: "2",
       name: "Burger King",
       src: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2024/12/16/a989969b-13f9-4931-8ebc-df9515dc8d2f_675289.jpg",
       cuisines: ["Burger", "Biryani", "Fast Food", "Snaks", "American"],
@@ -68,6 +70,7 @@ const resList = [
     type: "restaurant",
     data: {
       type: "F",
+      id: "3",
       name: "McDonald's",
       src: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2024/9/18/a6317ac5-d089-45bf-a392-c5384cbdd1e8_506982.jpg",
       cuisines: ["Burger", "Biryani", "Fast Food", "Snaks", "American"],
@@ -80,6 +83,7 @@ const resList = [
     type: "restaurant",
     data: {
       type: "F",
+      id: "4",
       name: "Domino's Pizza",
       src: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2024/10/1/c6f74d18-edb2-42d1-8f69-f91efb943c32_25396.jpg",
       cuisines: ["Burger", "Biryani", "Fast Food", "Snaks", "American"],
@@ -92,6 +96,7 @@ const resList = [
     type: "restaurant",
     data: {
       type: "F",
+      id: "5",
       name: "Cafe Arabia",
       src: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/FOOD_CATALOG/IMAGES/CMS/2024/3/11/cd029f04-9123-4304-ac0a-f242f4c64cb9_19bd9e19-b1d3-4010-90ed-e5187a93aeef.png_compressed",
       cuisines: ["Burger", "Biryani", "Fast Food", "Snaks", "American"],
@@ -104,6 +109,7 @@ const resList = [
     type: "restaurant",
     data: {
       type: "F",
+      id: "6",
       name: "Krushna Pure Veg",
       src: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/ohrooroghiooqsfytjri",
       cuisines: ["Burger", "Biryani", "Fast Food", "Snaks", "American"],
@@ -118,26 +124,9 @@ const Body = () => {
     <div className="body">
       <div className="search">Search</div>
       <div className="resto-container">
-        {/* Restaurantcard */}
-        <RestaurantCard
-          resData={resList[0]}
-          // resName="Meghana Foods"
-          // cuisine="Biryani,North Indian,Asian"
-          // stars="4.4 stars"
-          // minutes="38 minutes"
-        />
-        <RestaurantCard resData={resList[1]} />
-        <RestaurantCard resData={resList[2]} />
-        <RestaurantCard resData={resList[3]} />
-        <RestaurantCard resData={resList[4]} />
-        <RestaurantCard resData={resList[5]} />
-
-        {/* <RestaurantCard
-          resName="KFC"
-          cuisine="Burger,North Indian,Asian"
-          stars="4.5 stars"
-          minutes="40 minutes"
-        /> */}
+        {resList.map((restaurant) => (
+          <RestaurantCard key={resList.data.id} resData={restaurant} />
+        ))}
       </div>
     </div>
   );
